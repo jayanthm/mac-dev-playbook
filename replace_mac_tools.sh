e695cd3012cc73003a9d89852a4c583e3d8a185c
Jayanth Madheswaran <j@scale-data.com>
2015-03-28 16:01:39 -0700

    Add a script which replaces some mac unix tools in favor of the gnu version

---
+++ replace_mac_tools.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+sudo cp `which find` `which find`.bak
+sudo cp `which gfind` `which find`
+
+sudo cp `which dirname` `which dirname`.bak
+sudo cp `which gdirname` `which dirname`
+
+sudo cp `which readlink` `which readlink`.bak
+sudo cp `which greadlink` `which readlink`
